4b527fec81cd1ae63bc1478fc8b02f9145135f2f
jojojog <gaurav.k.work@gmail.com>
2024-07-14 15:03:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('invacker-sw.js')
-      .then(registration => console.log('Service Worker registered: ', registration.scope))
-      .catch(err => console.error('Service Worker registration failed: ', err));
-  });
-}
+
